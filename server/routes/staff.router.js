4797eec9da27ab282dee9d07c5e90f618c1233a9
Lars <larsmackenzie@gmail.com>
2018-10-05 15:11:48 -0500

    added routes to add employees and managers to junction tables

--- server/routes/staff.router.js
+++ server/routes/staff.router.js
@@ -13,12 +13,20 @@ router.get('/', (req, res) => {
 /**
  * POST routes 
  */
-// creates a new employee 
+// creates a new employee in the employee table
 router.post('/employee', (req, res) => {
 
 });
-// creates a new manager 
+// adds employee to employee_manager junction table
+router.post('/employee/junction', (req, res) => {
+
+});
+// creates a new manager in the person table 
 router.post('/manager', (req, res) => {
+
+});
+// add to the supervisor_manager junction table
+router.post('/manager/junction', (req, res) => {
 
 });
 /**
@@ -28,7 +36,7 @@ router.post('/manager', (req, res) => {
 router.put('/employee', (req, res) => {
 
 });
-// edits a manager's record 
+// edits a manager's record in person table 
 router.put('/manager', (req, res) => {
 
 });
@@ -41,6 +49,10 @@ router.delete('/employee', (req, res) => {
 });
 // deletes a manager 
 router.delete('/manager', (req, res) => {
+
+});
+// deletes a manager/employee relationship 
+router.delete('/manager/junction', (req, res) => {
 
 });
 module.exports = router;
