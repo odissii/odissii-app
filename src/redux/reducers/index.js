2202b2dd3cce39a97decf056a9571b4e9249d3cf
Lars <larsmackenzie@gmail.com>
2018-10-08 09:57:23 -0500

    imported feedback reducer to index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
+import feedback from './feedbackReducer';
 
 //Lets make a bigger object for our store, with the objects from our reducers.
 //This is why we get this.props.reduxStore.user.isLoading
 const store = combineReducers({
   user,
   login,
+  feedback
 });
 
 export default store;
