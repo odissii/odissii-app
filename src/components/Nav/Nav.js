1561dfc86a3d4e10893abcf6afb3117efddc88fd
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-10-09 13:15:32 -0500

    supervisor nav linking to paths

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,14 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import {USER_ACTIONS } from '../../redux/actions/userActions';
-import { triggerLogout } from '../../redux/actions/loginActions';
-import { BottomNavigation, BottomNavigationAction} from '@material-ui/core';
-import {group_add, person_add, creat, show_chart, menu } from '@material-ui/icons';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import Group from '@material-ui/icons/Group';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+import Create from '@material-ui/icons/Create';
+import ShowChart from '@material-ui/icons/ShowChart';
+import Menu from '@material-ui/icons/Menu';
+
 const mapStateToProps = state => ({
   user: state.user,
 })
@@ -14,87 +18,64 @@ class Nav extends Component {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
   }
 
-  componentDidUpdate() {
-    if (!this.props.user.isLoading && this.props.user.userName === null) {
-      this.props.history.push('home');
-    }
-  }
+  state = {
+    value: 'dashboard',
+  };
 
-  logout = () => {
-    this.props.dispatch(triggerLogout());
-  }
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
 
   render() {
     let content = null;
+    const { value } = this.state;
     // if supervisor logged in, will render appropriate nav bar
     if (this.props.user && this.props.user.role === 'supervisor') {
       content = (
         <div className="navbar">
-        <div>
-          <ul>
-            <li>
-              <Link to="/user">
-                User Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/dashboard">
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link to="/employees">
-                Employees
-              </Link>
-            </li>
-            <li>
-              <Link to="/feedback/new">
-                Give Feedback
-              </Link>
-            </li>
-            <li>
-              <Link to="/settings">
-                Settings
-              </Link>
-            </li>
-          </ul>
+          <BottomNavigation value={value} onChange={this.handleChange}>
+            <BottomNavigationAction icon={<ShowChart />} component={Link} to={"/dashboard"} />
+            <BottomNavigationAction icon={<Group />} component={Link} to={"/employees"}/>
+            <BottomNavigationAction icon={<Create />} component={Link} to={"/feedback/new"} />
+            <BottomNavigationAction icon={<Menu />} component={Link} to={"/settings"} />
+          </BottomNavigation>
         </div>
-      </div>
       )
       // if manager logged in, will render appropriate nav bar
     } else if (this.props.user && this.props.user.role_id === 'manager') {
       content = (
         <div className="navbar">
-    <div>
-      <ul>
-        <li>
-          <Link to="/user">
-            User Home
+          <div>
+            <ul>
+              <li>
+                <Link to="/user">
+                  User Home
           </Link>
-        </li>
-        <li>
-          <Link to="/dashboard">
-            Dashboard
+              </li>
+              <li>
+                <Link to="/dashboard">
+                  Dashboard
           </Link>
-        </li>
-        <li>
-          <Link to="/employees">
-            Employees
+              </li>
+              <li>
+                <Link to="/employees">
+                  Employees
           </Link>
-        </li>
-        <li>
-          <Link to="/feedback/new">
-            Give Feedback
+              </li>
+              <li>
+                <Link to="/feedback/new">
+                  Give Feedback
           </Link>
-        </li>
-        <li>
-          <Link to="/settings">
-            Settings
+              </li>
+              <li>
+                <Link to="/settings">
+                  Settings
           </Link>
-        </li>
-      </ul>
-    </div>
-  </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       )
     }
     return (
