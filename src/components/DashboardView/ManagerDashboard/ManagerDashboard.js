f3c231855b4e16ab9112b902eac3b1afcdf86353
Lars <larsmackenzie@gmail.com>
2018-10-09 14:48:02 -0500

    added donut chart of individual supervisor summary

--- src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
+++ src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography, Grid } from '@material-ui/core'; 
-import {Bar} from 'react-chartjs-2';
-const options = {
+import {Bar, Doughnut} from 'react-chartjs-2';
+const barOptions = {
   scales: {
        xAxes: [{
            stacked: true
@@ -12,42 +12,48 @@ const options = {
    }, 
 }
 const managerOverview = {
-  labels: ['Erin', 'Keith', 'Ben'],
+  labels: ['Erin', 'Keith', 'Ben', 'Maria', 'Mike'],
   datasets: [
     {
       label: 'Correct',
-      backgroundColor: '#ff0000',
-      data: [1, 5, 2]
+      backgroundColor: 'lightgray',
+      data: [1, 5, 2, 1, 3]
     },
     {
       label: 'Instruct',
-      backgroundColor: '#ffcc00',
-      data: [2, 3, 1]
+      backgroundColor: '#f17416',
+      data: [2, 3, 1, 4, 1]
     },
     {
       label: 'Praise',
-      backgroundColor: '#1dd043',
-      data: [4, 1, 4]
+      backgroundColor: '#0f77e6',
+      data: [4, 1, 4, 5, 2]
   },
-]
+]}
+const supervisorSummary = {
+    labels: ['Praise', 'Instruct', 'Correct'],
+    datasets: [{
+      data: [4, 2, 1],
+      backgroundColor: ['#0f77e6', '#f17416', 'lightgray']
+    }]
 }
 class ManagerDashboard extends React.Component {
   render(){
     return (
       <div>
           <Typography variant="display1">Manager's Dashboard</Typography>
-          <Bar data={managerOverview} options={options}/>
+          <Bar data={managerOverview} options={barOptions}/>
           <Typography variant="headline">Manager List</Typography>
       
       {/* this will be mapped from an array that contains all supervisors that this person oversees */}
       <Grid container spacing={0}>
           <Grid item xs={6}>
-          Manager 1 <br/>
+          Erin <br/>
           <a href="#summary">Summary</a><br/>
           <a href="#summary">Employees</a>
           </Grid>
           <Grid item xs={6}>
-          
+          <Doughnut data={supervisorSummary}/>
           </Grid>
       </Grid>
     </div>
