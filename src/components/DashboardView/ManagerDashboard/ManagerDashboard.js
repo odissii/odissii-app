1ccd6717d29ce7afcd9ed7f2ce69021b1e297e8d
Lars <larsmackenzie@gmail.com>
2018-10-09 11:37:28 -0500

    created basic manager dashboard elements

--- src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
+++ src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Typography, Grid } from '@material-ui/core'; 
 
 class ManagerDashboard extends React.Component {
   render(){
     return (
       <div>
-        This is the manager dashboard.
-      </div>
-    )
+          <Typography variant="headline">Manager's Dashboard</Typography>
+          <div>graph of all managers' feedback</div>
+          <Typography variant="headline">Manager List</Typography>
+      
+      {/* this will be mapped from an array that contains all supervisors that this person oversees */}
+      <Grid container spacing={0}>
+          <Grid item xs={6}>
+          Manager 1 
+          <a href="#summary">Summary</a>
+          <a href="#summary">Employees</a>
+          </Grid>
+          <Grid item xs={6}>
+          <div>graph</div>
+          </Grid>
+      </Grid>
+    </div>
+    );
   }
 }
 
