59881fbf16dbc75e30b6bba59924d49ff8f69f73
Lars <larsmackenzie@gmail.com>
2018-10-09 14:27:57 -0500

    basic stacked bar graph appearing on DOM

--- src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
+++ src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { Typography, Grid } from '@material-ui/core'; 
-import 
-
+import {Bar} from 'react-chartjs-2';
+const options = {
+  scales: {
+       xAxes: [{
+           stacked: true
+       }],
+       yAxes: [{
+           stacked: true
+       }]
+   }, 
+}
+const managerOverview = {
+  labels: ['Erin', 'Keith', 'Ben'],
+  datasets: [
+    {
+      label: 'Correct',
+      backgroundColor: '#ff0000',
+      data: [1, 5, 2]
+    },
+    {
+      label: 'Instruct',
+      backgroundColor: '#ffcc00',
+      data: [2, 3, 1]
+    },
+    {
+      label: 'Praise',
+      backgroundColor: '#1dd043',
+      data: [4, 1, 4]
+  },
+]
+}
 class ManagerDashboard extends React.Component {
   render(){
     return (
       <div>
           <Typography variant="display1">Manager's Dashboard</Typography>
-          <div className="graphPlaceholder">graph of all managers' feedback</div>
+          <Bar data={managerOverview} options={options}/>
           <Typography variant="headline">Manager List</Typography>
       
       {/* this will be mapped from an array that contains all supervisors that this person oversees */}
@@ -18,7 +47,7 @@ class ManagerDashboard extends React.Component {
           <a href="#summary">Employees</a>
           </Grid>
           <Grid item xs={6}>
-          <div>graph</div>
+          
           </Grid>
       </Grid>
     </div>
