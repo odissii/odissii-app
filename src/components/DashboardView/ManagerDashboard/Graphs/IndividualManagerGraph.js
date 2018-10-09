7009b5b7b7ef81ffbf50747d7134ffae5747c154
Lars <larsmackenzie@gmail.com>
2018-10-09 15:27:09 -0500

    moved graphs into components in manager dashboard view

---
+++ src/components/DashboardView/ManagerDashboard/Graphs/IndividualManagerGraph.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+const supervisorSummary = {
+    labels: ['Praise', 'Instruct', 'Correct'],
+    datasets: [{
+      data: [4, 2, 1],
+      backgroundColor: ['#0f77e6', '#f17416', 'lightgray']
+    }]
+}
+class IndividualManagerGraph extends Component {
+    render(){
+        return(
+            <div>
+                <Doughnut data={supervisorSummary}/>
+            </div>
+        );
+    }
+}
+export default IndividualManagerGraph; 
--- src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
+++ src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
@@ -1,64 +1,28 @@
 import React from 'react';
 import { Typography, Grid } from '@material-ui/core'; 
-import {Bar, Doughnut} from 'react-chartjs-2';
-const barOptions = {
-  scales: {
-       xAxes: [{
-           stacked: true
-       }],
-       yAxes: [{
-           stacked: true
-       }]
-   }, 
-}
-const managerOverview = {
-  labels: ['Erin', 'Keith', 'Ben', 'Maria', 'Mike'],
-  datasets: [
-    {
-      label: 'Correct',
-      backgroundColor: 'lightgray',
-      data: [1, 5, 2, 1, 3]
-    },
-    {
-      label: 'Instruct',
-      backgroundColor: '#f17416',
-      data: [2, 3, 1, 4, 1]
-    },
-    {
-      label: 'Praise',
-      backgroundColor: '#0f77e6',
-      data: [4, 1, 4, 5, 2]
-  },
-]}
-const supervisorSummary = {
-    labels: ['Praise', 'Instruct', 'Correct'],
-    datasets: [{
-      data: [4, 2, 1],
-      backgroundColor: ['#0f77e6', '#f17416', 'lightgray']
-    }]
-}
+import IndividualManagerGraph from './Graphs/IndividualManagerGraph';
+import ManagerOverviewGraph from './Graphs/ManagerOverviewGraph'; 
+
 class ManagerDashboard extends React.Component {
   render(){
     return (
       <div>
           <Typography variant="display1">Manager's Dashboard</Typography>
-          <Bar data={managerOverview} options={barOptions}/>
+              <ManagerOverviewGraph/>
           <Typography variant="headline">Manager List</Typography>
-      
       {/* this will be mapped from an array that contains all supervisors that this person oversees */}
       <Grid container spacing={0}>
-          <Grid item xs={6}>
-          Erin <br/>
-          <a href="#summary">Summary</a><br/>
-          <a href="#summary">Employees</a>
-          </Grid>
-          <Grid item xs={6}>
-          <Doughnut data={supervisorSummary}/>
+          <Grid item xs={7}>
+            <Typography variant="subheading">Erin</Typography>
+            <a href="#summary">Summary</a><br/>
+            <a href="#summary">Employees</a>
+            </Grid>
+          <Grid item xs={5}>
+            <IndividualManagerGraph />
           </Grid>
       </Grid>
     </div>
     );
   }
 }
-
 export default ManagerDashboard;
