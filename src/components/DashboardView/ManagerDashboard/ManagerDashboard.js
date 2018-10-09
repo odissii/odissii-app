55e3cc64bc596445e05ff8597a04cf812a0556b3
Lars <larsmackenzie@gmail.com>
2018-10-09 13:10:32 -0500

    edited styling

--- src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
+++ src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
@@ -5,15 +5,15 @@ class ManagerDashboard extends React.Component {
   render(){
     return (
       <div>
-          <Typography variant="headline">Manager's Dashboard</Typography>
-          <div>graph of all managers' feedback</div>
+          <Typography variant="display1">Manager's Dashboard</Typography>
+          <div className="graphPlaceholder">graph of all managers' feedback</div>
           <Typography variant="headline">Manager List</Typography>
       
       {/* this will be mapped from an array that contains all supervisors that this person oversees */}
       <Grid container spacing={0}>
           <Grid item xs={6}>
-          Manager 1 
-          <a href="#summary">Summary</a>
+          Manager 1 <br/>
+          <a href="#summary">Summary</a><br/>
           <a href="#summary">Employees</a>
           </Grid>
           <Grid item xs={6}>
