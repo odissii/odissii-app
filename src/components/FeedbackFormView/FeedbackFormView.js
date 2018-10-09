3f47f0deff0967aa41a3d5a04d313ca61819755d
Paul Heggeseth <pheggeseth@gmail.com>
2018-10-09 13:12:00 -0500

    added form buttons

--- src/components/FeedbackFormView/FeedbackFormView.js
+++ src/components/FeedbackFormView/FeedbackFormView.js
@@ -7,15 +7,16 @@ import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
+import FormGroup from '@material-ui/core/FormGroup';
 import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
-
+import Button from '@material-ui/core/Button';
 
 import Nav from '../Nav/Nav';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
-import { FormGroup } from '@material-ui/core';
+import { FEEDBACK_ACTIONS } from '../../redux/actions/feedbackActions';
 
 const mapStateToProps = state => ({
   user: state.user,
@@ -35,7 +36,6 @@ class FeedbackFormView extends React.Component {
       followUpDate: undefined,
     };
   }
-
 
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
@@ -63,78 +63,82 @@ class FeedbackFormView extends React.Component {
     console.log(formField, event.target.value);
   };
 
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log('form submitted:', this.state);
+  };
+
+  backToPreviousPage = event => {
+    console.log('back to previous page');
+  };
+
   render() {
     const {quality, taskRelated, cultureRelated, details, followUpNeeded, followUpDate} = this.state;
-    
-    // let followUpDateField;
-    // if (followUpNeeded) {
-    //   followUpDateField = (
-    //     <TextField 
-    //       label="Follow-Up Date"
-    //       type="date"
-    //     />
-    //   );
-      
-    // }
-    
+
     return (
       <div>
         <Nav />
         <div>
           This is the feedback form.
         </div>
-        <FormControl>
-          <FormLabel>Feedback Quality</FormLabel>
-          <RadioGroup
-            aria-label="feedback-type"
-            name="quality"
-            value={quality}
-            onChange={this.handleInputChange('quality')}
-          >
-            <FormControlLabel value="praise" label="Praise" control={<Radio />}/>
-            <FormControlLabel value="instruct" label="Instruct" control={<Radio />}/>
-            <FormControlLabel value="correct" label="Correct" control={<Radio />}/>
-          </RadioGroup>
-          <FormLabel>This feedback is:</FormLabel>
-          <FormGroup>
-            <FormControlLabel 
-              control={
-                <Switch 
-                  checked={taskRelated}
-                  onChange={this.handleInputChange('taskRelated')}
-                />
-              }
-              label="Task-Related"
-            />
-            <FormControlLabel 
-              control={
-                <Switch 
-                  checked={cultureRelated}
-                  onChange={this.handleInputChange('cultureRelated')}
-                />
-              }
-              label="Culture-Related"
-            />
-          </FormGroup>
-          <FormControlLabel 
-            label="Follow-Up Needed?"
-            control={
-              <Checkbox 
-                checked={followUpNeeded}
-                onChange={this.handleInputChange('followUpNeeded')}
+        <form onSubmit={this.handleFormSubmit}>
+          <FormControl>
+            <FormLabel>Feedback Quality</FormLabel>
+            <RadioGroup
+              aria-label="feedback-type"
+              name="quality"
+              value={quality}
+              onChange={this.handleInputChange('quality')}
+            >
+              <FormControlLabel value="praise" label="Praise" control={<Radio />}/>
+              <FormControlLabel value="instruct" label="Instruct" control={<Radio />}/>
+              <FormControlLabel value="correct" label="Correct" control={<Radio />}/>
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel>This feedback is:</FormLabel>
+            <FormGroup>
+              <FormControlLabel 
+                control={
+                  <Switch 
+                    checked={taskRelated}
+                    onChange={this.handleInputChange('taskRelated')}
+                  />
+                }
+                label="Task-Related"
               />
-            }
-          />
-          {/* follow-up date picker renders if the user checks the "Follow-Up Needed? box" */}
-          {followUpNeeded
-          ? <TextField 
-              type="date"
-              label="Follow-Up Date"
-              InputLabelProps={{
-                shrink: true,
-              }}
+              <FormControlLabel 
+                control={
+                  <Switch 
+                    checked={cultureRelated}
+                    onChange={this.handleInputChange('cultureRelated')}
+                  />
+                }
+                label="Culture-Related"
+              />
+            </FormGroup>
+          </FormControl>
+          <FormControl>
+            <FormControlLabel 
+              label="Follow-Up Needed?"
+              control={
+                <Checkbox 
+                  checked={followUpNeeded}
+                  onChange={this.handleInputChange('followUpNeeded')}
+                />
+              }
             />
-          : null}
+            {/* follow-up date picker renders if the user checks the "Follow-Up Needed? box" */}
+            {followUpNeeded
+            ? <TextField 
+                type="date"
+                label="Follow-Up Date"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            : null}
+          </FormControl>
           <TextField 
             label="Feedback Details"
             placeholder="Type or dictate feedback details"
@@ -142,7 +146,11 @@ class FeedbackFormView extends React.Component {
             onChange={this.handleInputChange('details')}
             multiline
           />
-        </FormControl>
+          <div>
+            <Button onClick={this.backToPreviousPage}>Cancel</Button>
+            <Button type="submit" color="primary" variant="raised">Submit</Button>
+          </div>
+        </form>
       </div>
       
     );
