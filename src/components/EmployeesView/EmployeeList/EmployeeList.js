7d137ddb6c1b68e88481006798b4284853d55e63
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-10-10 14:45:33 -0500

    adds code for supervisor employee list

--- src/components/EmployeesView/EmployeeList/EmployeeList.js
+++ src/components/EmployeesView/EmployeeList/EmployeeList.js
@@ -9,6 +9,7 @@ import { PEOPLE_ACTIONS } from '../../../redux/actions/peopleActions';
 const mapStateToProps = state => ({
     user: state.user,
     people: state.people.staff.allEmployees,
+    employees: state.people.staff.managerEmployees,
 })
 
 const space = ' ';
@@ -21,31 +22,56 @@ class EmployeeList extends React.Component {
 
     getEmployees = () => {
         if (this.props.user.role === USER_ROLES.MANAGER) {
-            this.props.dispatch({ type: PEOPLE_ACTIONS.FETCH_ALL_EMPLOYEES});
-        } else if (this.props.user.role === USER_ROLES.MANAGER) {
-            this.props.dispatch({ type: PEOPLE_ACTIONS.SET_MANAGER_EMPLOYEES});
+            this.props.dispatch({ type: PEOPLE_ACTIONS.FETCH_ALL_EMPLOYEES });
+        } else if (this.props.user.role === USER_ROLES.SUPERVISOR) {
+            this.props.dispatch({ type: PEOPLE_ACTIONS.SET_MANAGER_EMPLOYEES });
         }
     }
 
     render() {
-        return (
-            <Grid>
-                <div>
-                    <List>
-                        {this.props.people.map((employee) => {
-                            return <ListItem key={employee.id} value={employee}>
-                             <ListItemAvatar>
-                                <Avatar alt={employee.first_name} src={employee.image_path}/>
+        let content = null;
+        if (this.props.user.role === USER_ROLES.MANAGER) {
+            content = (
+                <List>
+                    {this.props.people.map((employee) => {
+                        return <ListItem key={employee.id} value={employee}>
+                            <ListItemAvatar>
+                                <Avatar alt={employee.first_name} src={employee.image_path} />
                             </ListItemAvatar>
-                            <ListItemText primary={employee.first_name + space + employee.last_name}/>
+                            <ListItemText primary={employee.first_name + space + employee.last_name} />
                             <ListItemSecondaryAction>
                                 <IconButton>
 
                                 </IconButton>
                             </ListItemSecondaryAction>
                         </ListItem>
-                        })}
-                    </List>
+                    })}
+                </List>
+            );
+        } else if (this.props.user.role === USER_ROLES.SUPERVISOR) {
+            content = (
+                <List>
+                    {this.props.employees.map((employee) => {
+                        return <ListItem key={employee.id} value={employee}>
+                            <ListItemAvatar>
+                                <Avatar alt={employee.first_name} src={employee.image_path} />
+                            </ListItemAvatar>
+                            <ListItemText primary={employee.first_name + space + employee.last_name} />
+                            <ListItemSecondaryAction>
+                                <IconButton>
+
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    })}
+                </List>
+            )
+        }
+
+        return (
+            <Grid>
+                <div>
+                    {content}
                 </div>
             </Grid>
         );
