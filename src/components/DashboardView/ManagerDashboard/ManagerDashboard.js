dd4fae8eb7fbd57564a165e99982e6a4f7d4d51b
Lars <larsmackenzie@gmail.com>
2018-10-16 09:37:02 -0500

    moved feedback data and supervisors to redux

--- src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
+++ src/components/DashboardView/ManagerDashboard/ManagerDashboard.js
@@ -5,6 +5,8 @@ import IndividualManagerGraph from './Graphs/IndividualManagerGraph';
 import ManagerOverviewGraph from './Graphs/ManagerOverviewGraph'; 
 import { connect } from 'react-redux';
 import { USER_ACTIONS } from '../../../redux/actions/userActions';
+import { PEOPLE_ACTIONS } from '../../../redux/actions/peopleActions'; 
+import { FEEDBACK_ACTIONS } from '../../../redux/actions/feedbackActions';
 import axios from 'axios';
 import './ManagerDashboard.css';
 import {CSVLink} from 'react-csv';
@@ -13,18 +15,15 @@ import moment from 'moment';
 const mapStateToProps = state => ({
   user: state.user,
   people: state.people,
-  feedback: state.feedback,
-  graphFeedback: state.feedback.supervisorOverview
+  feedback: state.feedback.feedback.feedbackCountsByAllSupervisors,
+  detailedFeedback: state.feedback.feedback.feedbackDetailsByAllSupervisors
 });
 
 class ManagerDashboard extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-        supervisors: [],
         sortedSupervisors: [],
-        feedbackData: [],
-        totalFeedback: [], 
         reports: {},
         praise: [],
         correct: [],
@@ -43,16 +42,14 @@ class ManagerDashboard extends React.Component {
       method: 'GET',
       url: '/api/staff/supervisors'
     }).then((response) => {
-      console.log(response.data); 
-      this.setState({
-        supervisors: response.data
-      });
+      this.props.dispatch({type: PEOPLE_ACTIONS.SET_SUPERVISORS, payload: response.data});
       this.getSupervisorFeedbackReports(response.data); 
-      this.sortSupervisors();
+      this.sortSupervisors(response.data);
     }).catch((error) => {
       console.log('Error getting supervisors', error); 
     })
   }
+  //this gets the past twelve months of feedback counting how many praise, instruct, and correct feedback each supervisor has given 
   getFeedbackCounts = (id) => {
     let today = new Date();
     let end = moment(today).format('L');
@@ -61,29 +58,31 @@ class ManagerDashboard extends React.Component {
       method: 'GET',
       url: `/api/feedback/supervisors/count?id=${id}&start=${start}&end=${end}`
     }).then((response) => {
-      this.setState({
-        feedbackData: [...this.state.feedbackData, response.data], 
-        totalFeedback: response.data
-      });
-        this.sortData(); 
-      
+      this.props.dispatch({type: FEEDBACK_ACTIONS.SET_ALL_FEEDBACK_BY_MANAGER_SUPERVISORS, payload: response.data});
+        this.sortData(response.data); 
       }).catch((error) => {
       console.log('Error getting feedback counts', error); 
     })
   }
+  //this gets more detailed reports about each feedback given by a supervisor, breaking each record down into employee, the details given, and the quality of the feedback
+  //this data is used in a CSV file which can be downloaded by a manager 
   getFeedbackDetails = (id) => {
+    let today = new Date();
+    let end = moment(today).format('L');
+    let start = moment(today).subtract(1, 'year').format('L');  
     axios({
       method: 'GET',
-      url: `/api/feedback/supervisors/reports?id=${id}`
+      url: `/api/feedback/supervisors/reports?id=${id}&start=${start}&end=${end}`
     }).then((response) => {
-      console.log(response.data);
-      this.setState({
-          reports: {...this.state.reports, [id]: response.data}
-        });
+      this.props.dispatch({type: FEEDBACK_ACTIONS.SET_ALL_DETAILED_FEEDBACK_BY_MANAGER_SUPERVISORS, payload: response.data, supervisor: id});
+      // this.setState({
+      //     reports: {...this.state.reports, [id]: response.data}
+      //   });
       }).catch((error) => {
       console.log('Error getting feedback details', error); 
     })
   }
+  //this sorts the feedback and extracts each supervisor ID to send to two functions which GET two sets of feedback data 
   getSupervisorFeedbackReports = (array) => {
     for (let i = 0; i < array.length; i++){
       let id = array[i].supervisor_id; 
@@ -91,20 +90,19 @@ class ManagerDashboard extends React.Component {
       this.getFeedbackDetails(id);
     }
   }
-  sortSupervisors = () => {
-    for(let i = 0; i < this.state.supervisors.length; i++){
-      console.log(this.state.supervisors[i]);
+  sortSupervisors = (array) => {
+    for(let i = 0; i < array.length; i++){
       this.setState({
-        sortedSupervisors: [...this.state.sortedSupervisors, this.state.supervisors[i].first_name + ' ' + this.state.supervisors[i].last_name,]
+        sortedSupervisors: [...this.state.sortedSupervisors, array[i].first_name + ' ' + array[i].last_name]
       })
     }
   }
-  sortData = () => {
-    for(let i = 0; i < this.state.totalFeedback.length; i++){
+  sortData = (array) => {
+    for(let i = 0; i < array.length; i++){
           this.setState({
-            praise: [...this.state.praise, parseInt(this.state.totalFeedback[i].praise)],
-            correct: [...this.state.correct, parseInt(this.state.totalFeedback[i].correct)],
-            instruct: [...this.state.instruct, parseInt(this.state.totalFeedback[i].instruct)],
+            praise: [...this.state.praise, parseInt(array[i].praise)],
+            correct: [...this.state.correct, parseInt(array[i].correct)],
+            instruct: [...this.state.instruct, parseInt(array[i].instruct)],
           })   
         }
     }
@@ -112,6 +110,7 @@ class ManagerDashboard extends React.Component {
   render(){
     return (
       <div className="padding-bottom">
+      {JSON.stringify(this.props.feedback)}
         <Grid container spacing={0}>
         <Grid item xs={12}>
           <h1>Manager's Dashboard</h1>
@@ -123,7 +122,7 @@ class ManagerDashboard extends React.Component {
           <Grid item xs={12}>
               <h2 className="center">All Supervisors</h2>
           </Grid>
-                {this.state.feedbackData.map((array, i) => {
+                {this.props.feedback.map((array, i) => {
                         return(
                         <div key={i}>
                          {array.map((feedback, j)=> {
@@ -135,7 +134,7 @@ class ManagerDashboard extends React.Component {
                                       <Button color ="primary" onClick={()=>this.props.history.push('/employees')}>Employees</Button>
                                        <p>Feedback given past 12 months</p>
                                         <IndividualManagerGraph feedback={feedback}/> 
-                                        {this.state.reports[feedback.sid] && <CSVLink data={this.state.reports[feedback.sid]}
+                                        {this.props.detailedFeedback[feedback.sid] && <CSVLink data={this.props.detailedFeedback[feedback.sid]}
                                           filename={`${feedback.last_name}-feedback.csv`}
                                           target="_blank"
                                         >Download CSV</CSVLink>}
