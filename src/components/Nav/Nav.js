f627741bf96f7123df65631fa3e270d4ce992b1e
Lars <larsmackenzie@gmail.com>
2018-10-16 15:58:45 -0500

    edited link to add new people

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -50,7 +50,7 @@ class Nav extends Component {
           <BottomNavigation value={value} style={styles.stickToBottom} onChange={this.handleChange}>
             <BottomNavigationAction className="nav" icon={<ShowChart />} component={Link} to={"/dashboard"} />
             <BottomNavigationAction className="nav" icon={<Group />} component={Link} to={"/allEmployees"} />
-            <BottomNavigationAction className="nav" icon={<PersonAdd />} component={Link} to={"/employee/new"} />
+            <BottomNavigationAction className="nav" icon={<PersonAdd />} component={Link} to={"/addperson"} />
             <BottomNavigationAction className="nav" icon={<Menu />} component={Link} to={"/settings"} />
           </BottomNavigation>
         </div>
