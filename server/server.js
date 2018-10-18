0c104759fc7bc35ce913d4f57b67dfb2a30e52cc
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-10-18 10:29:12 -0500

    adds arrow to the sort function

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ const userRouter = require('./routes/user.router');
 const feedbackRouter = require('./routes/feedback.router');
 const followupRouter = require('./routes/followup.router');
 const staffRouter = require('./routes/staff.router'); 
-const nodemailerRouter = require('./routes/nodemailer.router');
 const qualityRouter = require('./routes/quality.router');
 
 
