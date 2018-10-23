fe8a008dbb7aaf23e0403f1d92bc1e7414efe931
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-10-23 09:21:57 -0500

    added padding and background color to list

--- src/components/EmployeesView/EmployeeList/AllEmployeeList.js
+++ src/components/EmployeesView/EmployeeList/AllEmployeeList.js
@@ -23,6 +23,8 @@ const mapStateToProps = state => ({
 const styles = {
     table: {
         marginTop: 5,
+        paddingBottom: '20vh',
+        backgroundColor: 'white',
     },
     tableCell: {
         padding: 0,
